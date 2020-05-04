5b478756a0d8033c6c84ad2557dc02adeb027c25
Milessa <60219725+Milessa@users.noreply.github.com>
2020-05-04 16:44:23 +0300

    Adden new gulp plugin
    Gulp plugin that minimizes css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 const gulp = require("gulp");
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task("hello", function(done) {
-  console.log("Hello  world!");
-  done();
-});
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -14,4 +11,11 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('minify-css', () => {
+  return gulp.src('./*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
